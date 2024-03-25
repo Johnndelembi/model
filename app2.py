a5bd7eacd6a50c632e818a5f431b55eeed252498
Johnndelembi <159067569+Johnndelembi@users.noreply.github.com>
2024-03-25 21:01:21 +0300

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+from transformers import pipeline
+
+model = pipeline("table-question-answering", model="google/tapas-base-finetuned-wtq")
+df = pd.read_csv("vgsales.csv")
+table = df.astype(str)
+
+st.title("Chat with your data")
+st.write(table.head())
+
+query = st.text_area("Ask me anything about the dataset and i will answer you")
+output = model(table=table, query=query)['answer']
+st.write(output)
+    
+
